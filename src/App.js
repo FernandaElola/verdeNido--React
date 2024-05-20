f4307a74316d34bf1bbbe477dddb81e9591990fb
Diego Lalanda <guaio_2014@hotmail.com>
2024-05-19 21:43:26 -0300

    Agrego About, Boton Flotante, y animacion Nav

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Nav } from './components/Nav';
 import { Main } from './components/Main';
 import { Footer } from './components/Footer';
 import { Contact } from './components/Contact';
+import About from './components/About';
 
 function App() {
   return (
@@ -12,10 +13,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
           <Route path="/main" element={<Main nroClase="8" tituloTema="Como hacer uso de las props" />} />
         </Routes>
       </>
-    </Router>
+    </Router> 
   );
 }
 
---
+++ src/components/FloatingButton.jsx
@@ -0,0 +1,12 @@
+
+import './FloatingButton.css'; // Importa el archivo de estilos CSS para el botón flotante
+
+const FloatingButton = () => {
+  return (
+    <div className="floating-button">
+      <img src="img/BOTON-RESERVA.png" alt="Reserva Aquí" />
+    </div>
+  );
+}
+
+export default FloatingButton;
