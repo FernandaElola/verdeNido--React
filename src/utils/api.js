a353a8a0819dccd8635910021f03abb213f52288
Diego Lalanda <guaio_2014@hotmail.com>
2024-06-13 23:45:50 -0300

    agrego fetchImagenesByAlojamientoId

--- src/utils/api.js
+++ src/utils/api.js
@@ -20,4 +20,35 @@ export const fetchAlojamientos = async () => {
   }
 };
 
+export const fetchImagenesByAlojamientoId = async () => {
+  try {
+    const response = await fetch(`http://localhost:3001/imagen/getAllImagenes`);
+    if (!response.ok) {
+      throw new Error('Error al obtener las imágenes');
+    }
+    const data = await response.json();
+
+    // Objeto para almacenar imágenes agrupadas por idAlojamiento
+    const imagenesPorAlojamiento = {};
+
+    // Organizar las imágenes por idAlojamiento
+    data.forEach(imagen => {
+      const { idImagen, idAlojamiento, RutaArchivo } = imagen;
+      if (!imagenesPorAlojamiento[idAlojamiento]) {
+        imagenesPorAlojamiento[idAlojamiento] = [];
+      }
+      imagenesPorAlojamiento[idAlojamiento].push({ idImagen, RutaArchivo });
+    });
+
+    console.log('Imágenes por idAlojamiento:', imagenesPorAlojamiento);
+
+    return imagenesPorAlojamiento;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error;
+  }
+};
+
+
+
 
